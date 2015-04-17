a5e33b1ef0f6b62f953404939031750da1b487ab
David Liebemann <David.Liebemann@gmail.com>
2015-04-17 10:16:57 +0200

    Initialised LibGDX Added Game.java as the Applicationlistener

--- LudumDare_DT/src/de/ludumDare_DT/ludumDare_DT_2015/Main.java
+++ LudumDare_DT/src/de/ludumDare_DT/ludumDare_DT_2015/Main.java
@@ -1,8 +1,16 @@
 package de.ludumDare_DT.ludumDare_DT_2015;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import de.ludumDare_DT.ludumDare_DT_2015.Game;
+
 public class Main {
 	public static void main(String[] args) {
-		
-	}
-
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "LudumDare_DT_2015";
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new Game(), config);
+    }
 }

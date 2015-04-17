2b38f6f8c767e9f43db3b3cd223efbea10a35413
meiert <meiert@hochschule-trier.de>
2015-04-17 11:50:39 +0200

    Input allgemein drin

---
+++ LudumDare_DT/src/de/ludumDare_DT/ludumDare_DT_2015/input/InputPuffer.java
@@ -0,0 +1,7 @@
+package de.ludumDare_DT.ludumDare_DT_2015.input;
+
+import java.util.HashMap;
+
+public class InputPuffer {
+	HashMap<InputKey, Integer> puffer = new HashMap<InputKey, Integer>();
+}

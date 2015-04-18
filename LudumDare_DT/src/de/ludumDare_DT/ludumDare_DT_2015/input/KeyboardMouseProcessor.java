2b932309c43d2a0fdba2a74785110ec9c570494c
meiert <meiert@hochschule-trier.de>
2015-04-18 09:00:48 +0200

    input auf spiel angepasst

--- LudumDare_DT/src/de/ludumDare_DT/ludumDare_DT_2015/input/KeyboardMouseProcessor.java
+++ LudumDare_DT/src/de/ludumDare_DT/ludumDare_DT_2015/input/KeyboardMouseProcessor.java
@@ -3,13 +3,17 @@ package de.ludumDare_DT.ludumDare_DT_2015.input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
 
-import de.ludumDare_DT.ludumDare_DT_2015.game.Game;
-
 public class KeyboardMouseProcessor implements InputProcessor {
 
 	@Override
 	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
+		System.out.println(keycode);
+		switch(keycode) {
+			case 29: InputPuffer.puffer.put(InputKey.MOVE_LEFT, 1.0f);
+			case 32: InputPuffer.puffer.put(InputKey.MOVE_RIGHT, 1.0f);
+			case 47: InputPuffer.puffer.put(InputKey.MOVE_DOWN, 1.0f);
+			case 51: InputPuffer.puffer.put(InputKey.MOVE_UP, 1.0f);
+		}
 		return false;
 	}
 
@@ -27,7 +31,9 @@ public class KeyboardMouseProcessor implements InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {			
-		InputPuffer.puffer2.put(InputKey.MOUSE_CLICK_DOWN, new Vector2(screenX, screenY));
+		switch(button) {
+			case 0: InputPuffer.puffer2.put(InputKey.MOUSE_LEFT, new Vector2(screenX, screenY));
+		}
 		return true;
 	}
 
